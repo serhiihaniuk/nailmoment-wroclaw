5edc7e74b4a89591f92fb519a75859af60f5b2c5
serhiihaniuk <serhii.haniuk@gmail.com>
2025-04-26 17:29:15 +0200

    upd link outline

--- blocks/ui/link.tsx
+++ blocks/ui/link.tsx
@@ -20,7 +20,7 @@ export const Link: React.FC<LinkWithIconProps> = ({
     <a
       target={target}
       rel="noopener noreferrer"
-      className={`underline text-blue-foreground inline-flex items-center gap-1 ${className}`}
+      className={`underline text-blue-foreground inline-flex items-center gap-1 outline-0 focus-visible:ring-[3px] rounded-md focus-visible:border-ring focus-visible:ring-ring/50 ${className}`}
       href={href}
     >
       {children}
