f79b61d49fe8d3d8e4433f796f37787c4052ca15
serhiihaniuk <serhii.haniuk@gmail.com>
2026-02-28 15:34:31 +0100

    update next js, react js

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,10 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+import nextConfig from "eslint-config-next";
 
 const eslintConfig = [
-  // Apply the Next.js recommended configurations first
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-
-  // Add a new configuration object to override specific rules
+  ...nextConfig,
   {
     rules: {
-      // Disable the no-img-element rule from @next/next plugin
       "@next/next/no-img-element": "off",
-
-      // You can add other rule modifications here if needed
-      // Example: "react/react-in-jsx-scope": "off",
     },
   },
 ];
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   images: {
     remotePatterns: [
       new URL("https://oet9iwqxtk87xaxw.public.blob.vercel-storage.com/**"),
